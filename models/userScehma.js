2d4ddd024ed1bbf9dae2a98a031fa8103ace4e8c
Botezatu <george.botezatu00@e-uvt.ro>
2021-11-27 15:47:04 +0200

    Created userSchema

---
+++ models/userScehma.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		require: true,
+	},
+	avatar: {
+		type: String,
+	},
+	data: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const User = mongoose.model("user", userSchema);
+export default User;
