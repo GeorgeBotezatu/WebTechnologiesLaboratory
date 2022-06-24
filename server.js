427098dc142967b29cf25c086975228afa49a541
Botezatu <george.botezatu00@e-uvt.ro>
2022-06-24 10:39:34 +0300

    prepare for  deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import path from "path";
 //routes
 import userRoutes from "./routes/userRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
@@ -34,6 +35,15 @@ app.use("/api/console", consoleRoutes);
 app.use("/api/courses", coursesRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+
+//
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}...`.yellow.bold));
