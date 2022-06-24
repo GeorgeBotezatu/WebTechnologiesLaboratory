c1b4456517e621080b5cf503089d523f1ad6f7c0
Botezatu <george.botezatu00@e-uvt.ro>
2022-06-24 11:13:33 +0300

    fix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 //routes
 import userRoutes from "./routes/userRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
