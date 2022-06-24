0c9443ca67b7e2cfc4516b90815fbd5ce1f8b2a0
Botezatu <george.botezatu00@e-uvt.ro>
2022-06-24 11:05:48 +0300

    fix

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use("/api/dashboard", dashboardRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
+	const __dirname = path.dirname(fileURLToPath(import.meta.url));
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
